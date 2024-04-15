1777cbdfbf6691db4378bc5af62aa1b437a76076
Murr, Alexander <acmurr@hudsonproducts.com>
2024-04-15 08:51:31 -0400

    Added "Bounty" project. The idea for this is code that is not working that we want to share to each other to collectively solve.

---
+++ Bounty/ChangeStructureMemberSize.cs
@@ -0,0 +1,25 @@
+﻿using SolidWorks.Interop.sldworks;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Bounty
+{
+    internal class ChangeStructureMemberSize
+    {
+        static void Main()
+        {
+            SldWorks SW = (SldWorks)Marshal.GetActiveObject("SldWorks.Application");
+            ModelDoc2 modelDoc2 = SW.IActiveDoc2;
+            var selectionMgr = modelDoc2.SelectionManager;
+
+            string newSize = "W8x28";
+
+            Feature feature = selectionMgr.GetSelectedObject6(1, -1) as Feature;
+            IStructuralMemberFeatureData member = feature.GetDefinition() as IStructuralMemberFeatureData;
+
+            Console.WriteLine($"Attempting to set {member.ConfigurationName} to {newSize}");
+            member.ConfigurationName = newSize;
+            Console.WriteLine($"  Result: {member.ConfigurationName}");
+        }
+    }
+}
