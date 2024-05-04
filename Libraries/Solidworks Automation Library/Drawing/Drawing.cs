e7bc266dccc4602ae36eeef4632ddd02f6261c3a
Murr, Alexander <acmurr@hudsonproducts.com>
2024-05-03 20:56:00 -0400

    deleted export files

--- Libraries/Solidworks Automation Library/Drawing/Drawing.cs
+++
@@ -1,9 +0,0 @@
-﻿using SolidWorks.Interop.sldworks;
-
-namespace Automation_Library
-{
-    public class Drawing : SolidWorksData
-    {
-        public static IDrawingDoc drawingDoc = (IDrawingDoc)modelDoc2;
-    }
-}
